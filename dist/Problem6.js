3f4e36804296cc3bd18163e6be5426ca6fecc9fc
Kakon Ray <thisiskakonray@gmail.com>
2024-11-07 00:22:45 +0600

    Problem 6 Completed

---
+++ dist/Problem6.js
@@ -0,0 +1,6 @@
+"use strict";
+const updateProfile = (obj, update) => {
+    return Object.assign(Object.assign({}, obj), update);
+};
+const myProfile = { name: "Alice", age: 25, email: "alice@example.com" };
+console.log(updateProfile(myProfile, { age: 26 }));
